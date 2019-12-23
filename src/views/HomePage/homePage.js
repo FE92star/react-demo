ff70638b0f9cccc5dbf0117ea325047186350be3
bao mao <maobao@broada.com>
2019-12-23 10:02:12 +0800

    update

--- src/views/HomePage/homePage.js
+++ src/views/HomePage/homePage.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useMemo, useCallback, useRef, Component } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './homePage.less'
 
-function ListData() { // 无状态组件
-  const listData = [1,1,1,1,1,1,1]
-  return (
-    listData.map((item, index) => {
-      return (
-        <li key={ index }>{ item }</li>
-      )
-    })
-  )
+// function ListData(props) { // 无状态组件
+//   const listData = [1,1,1,1,1,1,1]
+//   const { name } = props
+//   return (
+//     listData.map((item, index) => {
+//       return (
+//         <li key={ index }>{ name }</li>
+//       )
+//     })
+//   )
+// }
+
+class ListData extends Component {
+  constructor(props) {
+    super(props)
+    this.props = props
+  }
+  componentDidMount() {
+    // console.log(this.props.name)
+  }
+  componentDidUpdate(prevProps) {
+    console.log(prevProps, this.props)
+  }
+  render() {
+    return (
+      <p>66666</p>
+    )
+  }
 }
 
 export default class HomePage extends Component {
   state = {
-    data: 'home'
+    data: 'home',
+    obj: {
+      name: 'state',
+      title: 'good'
+    }
   }
   componentDidMount() {
     this.setState({
@@ -24,16 +47,93 @@ export default class HomePage extends Component {
       console.log(this.state.data)
     })
   }
+  handleClick = () => {
+    this.setState({
+      obj: {
+        name: 'new state'
+      }
+    })
+  }
   render() {
     // const listData = [1,1,1,1,1,1,1]
     return (
-    <div className="app">
-      <h2>{ this.state.data }</h2>
-      <Link to="/detail">detail</Link>
-      <div></div>
-      <NavLink to="/login">login</NavLink>
-      <ListData />
-    </div>
+      <div className="app">
+        <button onClick={this.handleClick}></button>
+        <h2>{ this.state.data }</h2>
+        <Link to="/detail">detail</Link>
+        <div></div>
+        <NavLink to="/login">login</NavLink>
+        <ListData name={this.state.obj} />
+      </div>
     )
   }
 }
+// export function ListData (props) {
+//   const { name } = props
+//   const [list, setList] = useState([])
+//   const [count, setCount] = useState(1)
+//   const listMap = useMemo(() => {
+//     console.log('render', new Date().getTime())
+//     return count * 10
+//   }, [count])
+//   const fetchData = useCallback(async () => {
+//     const listData = await function () {
+//       const data = [1, 2, 3]
+//       return data
+//     }
+//     setList(listData)
+//   }, [])
+//   useEffect(() => {
+//     fetchData()
+//   }, [fetchData])
+//   useEffect(() => {
+//     setCount(10)
+//     console.log(count, 'count')
+//   })
+//   useEffect(() => {
+//     // console.log('effect hooks')
+//   }) 
+//   const listStyle = {
+//     color: '#000'
+//   }
+//   console.log('reRender', new Date().getTime())
+//   return (
+//     <div>
+//       <p>{ name }</p>
+//       {
+//         list.map((item, index) => {
+//           return (
+//             <React.Fragment key={index}>
+//               <li style={listStyle}>{ item }</li>
+//               <li style={listStyle}>{ listMap }</li>
+//             </React.Fragment>
+//           )
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+// export default function HomePage (props) {
+//   const [title, setTitle] = useState('')
+//   const [name, setName] = useState('Bob')
+//   const count = useRef(0)
+//   useEffect(() => {
+//     const domTitle = document.title
+//     setTitle(domTitle)
+//   }, [])
+
+//   const handleClick = () => {
+//     count.current++
+//     setTimeout(() => {
+//       console.log(count)
+//     }, 3000)
+//   }
+//   return (
+//     <div>
+//       <button onClick={handleClick}>change</button>
+//       { title }
+//       <ListData name={name} />
+//     </div>
+//   )
+// }
